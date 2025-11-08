d3e5137c712920186099b0c5c077053b36333e46
ayazhan.k <a_karatayeva@kbtu.kz>
2025-11-08 20:58:00 +0500

    splash screen of the app
    I added splash screen and made sure it doesn't crash the program

--- app/src/main/java/com/example/kbtu_helper/SplashScreen.kt
+++ app/src/main/java/com/example/kbtu_helper/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.kbtu_helper
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,6 +11,8 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
